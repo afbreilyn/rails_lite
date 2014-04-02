0775a643e20ad0ef0eb0c3459668f6733ac122d3
aaron foster bresley <afbresley@gmail.com>
2014-04-02 15:34:54 -0400

    Creates simple server. Adds ControllerBase::initialize, ::render_content, ::alread_built_response?, ::redirect_to, and ::render methods

--- lib/rails_lite/controller_base.rb
+++ lib/rails_lite/controller_base.rb
@@ -9,25 +9,51 @@ class ControllerBase
 
   # setup the controller
   def initialize(req, res, route_params = {})
+    @req = req
+    @res = res
+
+    @already_built_response = false
   end
 
   # populate the response with content
   # set the responses content type to the given type
   # later raise an error if the developer tries to double render
   def render_content(content, type)
+    raise "error" if already_built_response?
+
+    @res.body = content
+    @res.content_type = type
+
+    @already_built_response = true
   end
 
   # helper method to alias @already_built_response
   def already_built_response?
+    #raise "Error" if @already_built_response == true
+    @already_built_response
   end
 
   # set the response status code and header
   def redirect_to(url)
+    raise "error" if already_built_response?
+
+    res.status = 302
+    res.header["location"] = url
+    #res.setup_header(url) # res.header = url
+    @already_built_response = true
+
   end
 
   # use ERB and binding to evaluate templates
   # pass the rendered html to render_content
   def render(template_name)
+    raise "error" if already_built_response?
+    controller_name = "#{self.class}".underscore
+    file = File.read("views/#{controller_name}/#{template_name}.html.erb")
+    content = ERB.new(file).result(binding)
+
+    render_content(content, "text/html")
+    @already_built_response = true
   end
 
   # method exposing a `Session` object
