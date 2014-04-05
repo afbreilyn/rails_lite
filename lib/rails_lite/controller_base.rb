d9e423bfbe35ac4abc668f20d3fd1d80e1727082
afbresley <afbresley@gmail.com>
2014-04-04 20:59:57 -0400

    fixes last rspec
    implemented ||= instead of || in controller_base

--- lib/rails_lite/controller_base.rb
+++ lib/rails_lite/controller_base.rb
@@ -1,7 +1,7 @@
 require 'erb'
-require 'active_support/inflector'
 require_relative 'params'
 require_relative 'session'
+require 'active_support/inflector'
 
 
 class ControllerBase
@@ -17,17 +17,9 @@ class ControllerBase
     @params = Params.new(req, route_params)
   end
 
-  # populate the response with content
-  # set the responses content type to the given type
-  # later raise an error if the developer tries to double render
-  def render_content(content, type)
-    raise "error" if already_built_response?
-
-    @res.body = content
-    @res.content_type = type
-
-    @already_built_response = true
-    nil
+  # method exposing a `Session` object
+  def session
+    @session ||= Session.new(@req)
   end
 
   # helper method to alias @already_built_response
@@ -40,36 +32,49 @@ class ControllerBase
   def redirect_to(url)
     raise "error" if already_built_response?
 
-    res.status = 302
-    res.header["location"] = url
+    @res.status = 302
+    @res.header["location"] = url
     #res.setup_header(url) # res.header = url
-    @already_built_response = true
+    session.store_session(@res)
 
+    @already_built_response = true
+    nil
   end
+  
+  # populate the response with content
+  # set the responses content type to the given type
+  # later raise an error if the developer tries to double render
+  def render_content(content, type)
+    raise "error" if already_built_response?
+
+    @res.body = content
+    @res.content_type = type
+    session.store_session(@res)
+
+    @already_built_response = true
+    nil
+  end
+
 
   # use ERB and binding to evaluate templates
   # pass the rendered html to render_content
   def render(template_name)
-    raise "error" if already_built_response?
-    controller_name = "#{self.class}".underscore
-    file = File.read("views/#{controller_name}/#{template_name}.html.erb")
-    content = ERB.new(file).result(binding)
+    # raise "error" if already_built_response?
+    # controller_name = "#{self.class}".underscore
+    # file = File.read("views/#{controller_name}/#{template_name}.html.erb")
+    # content = ERB.new(file).result(binding)
 
-    render_content(content, "text/html")
-    @already_built_response = true
+    # render_content(content, "text/html")
+    # @already_built_response = true
 
-    # template_fname=
-    #   File.join("views", self.class.name.underscore, "#{template_name}.html.erb")
-    # render_content(
-    #   ERB.new(File.read(template_fname)).result(binding),
-    #   "text/html"
-    # )
+    template_fname=
+      File.join("views", self.class.name.underscore, "#{template_name}.html.erb")
+    render_content(
+      ERB.new(File.read(template_fname)).result(binding),
+      "text/html"
+    )
   end
 
-  # method exposing a `Session` object
-  def session
-    @session || Session.new(@req)
-  end
 
   # use this with the router to call action_name (:index, :show, :create...)
   def invoke_action(name)
